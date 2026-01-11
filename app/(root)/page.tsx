269cd81d2d9adf3b7613a42977c6d2dbb3f637c0
rayliu7717 <rayliu2004@gmail.com>
2026-01-10 22:29:27 -0500

    Sample Products & Product List

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
 const delay = (ms: number | undefined) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
 const Homepage = async () => {
-  await delay(2000);
-  return <>GStore</>;
+  //  await delay(2000);
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
