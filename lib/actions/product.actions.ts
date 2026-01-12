6f6f970a94dd82123a9f98bf30ccb3014f81ac08
rayliu7717 <rayliu2004@gmail.com>
2026-01-11 19:55:17 -0500

    Product Detail Page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,18 +1,21 @@
 "use server";
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/lib/generated/prisma/client";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { prisma } from "@/db/prisma";
 
 // Get latest product
 export async function getLatestProducts() {
-  const connectionString = `${process.env.DATABASE_URL}`;
-  const adapter = new PrismaPg({ connectionString });
-  const prisma = new PrismaClient({ adapter });
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
   return convertToPlainObject(data);
+}
+
+// Get single product by slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 }
