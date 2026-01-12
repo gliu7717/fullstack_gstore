980f0cd56290efa1ad8a44576c011cdccc35dff2
rayliu7717 <rayliu2004@gmail.com>
2026-01-11 20:19:02 -0500

    make image clickable

--- components/shared/product/product-images.tsx
+++ components/shared/product/product-images.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
 const ProductImages = ({ images }: { images: string[] }) => {
   const [current, setCurrent] = useState(0);
@@ -16,7 +17,14 @@ const ProductImages = ({ images }: { images: string[] }) => {
       />
       <div className='flex'>
         {images.map((image, index) => (
-          <div key={image}>
+          <div
+            key={image}
+            onClick={() => setCurrent(index)}
+            className={cn(
+              "border mr-2 cursor-pointer hover:border-orange-600",
+              current === index && "border-orange-500"
+            )}
+          >
             <Image src={image} alt='image' width={100} height={100} />
           </div>
         ))}
