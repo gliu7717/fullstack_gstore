2ceaa0f5b5190a1430934d63acd35ea8c7b01c4d
rayliu7717 <liu.geyang@gmail.com>
2026-02-16 20:47:23 -0500

    add-to-cart component

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { getProductBySlug } from "@/lib/actions/product.actions";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import ProductImages from "@/components/shared/product/product-images";
+import AddToCart from "@/components/shared/product/add-to-cart";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -67,9 +68,13 @@ const ProductDetailsPage = async (props: {
                     <Badge variant='destructive'>Unavailable</Badge>
                   )}
                 </div>
+
                 {product.stock > 0 && (
                   <div className=' flex-center'>
-                    <Button className='w-full'>Add to cart</Button>
+                    <AddToCart item={{
+                      productId: product.id, name: product.name, slug: product.slug,
+                      price: Number(product.price), qty: 1, image: product.images![0]
+                    }} />
                   </div>
                 )}
               </CardContent>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constants";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
