7ce3591a2b0a44ffaa619ef95d8a62d6c70db26c
rayliu7717 <liu.geyang@gmail.com>
2026-02-16 19:55:38 -0500

    cart-schema and model

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,7 +4,7 @@ import { signIn, signOut } from '@/auth'
 import { isRedirectError } from "next/dist/client/components/redirect-error"
 import { hashSync } from "bcrypt-ts-edge"
 import { prisma } from '@/db/prisma'
-import { success } from "zod"
+import { formatError } from "../utils"
 
 // Sign in the user with credentials
 export async function signInWithCredentials(preState: unknown, formData: FormData) {
@@ -17,10 +17,13 @@ export async function signInWithCredentials(preState: unknown, formData: FormDat
         return { success: true, message: 'Signed in successfully' }
     } catch (error) {
 
+        console.log(error);
+        //console.log(error.flatten().fieldErrors);
+
         if (isRedirectError(error)) {
             throw error
         }
-        return { success: false, message: 'Invalid email or password' }
+        return { success: false, message: formatError(error) }
     }
 }
 
