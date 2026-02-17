4b9e42b399eb51e50048d71555144620fb4b65f0
rayliu7717 <liu.geyang@gmail.com>
2026-02-16 22:22:25 -0500

    fixed the ts compiling error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,10 +4,11 @@ import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts } from "@/lib/actions/product.actions";
 
 export const dynamic = "force-dynamic";
+import { Product } from "@/types";
 
 const Homepage = async () => {
   //  await delay(2000);
-  const latestProducts = await getLatestProducts();
+  const latestProducts = await getLatestProducts() as Product[];
   return (
     <>
       <ProductList data={latestProducts} title='Newest Arrivals' />
