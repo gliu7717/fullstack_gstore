848bc9ffa2b0c1a5443d7984c019331b1ba0b522
rayliu7717 <liu.geyang@gmail.com>
2026-02-20 23:21:34 -0500

    shipping address page

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -16,3 +16,10 @@ export const signUpDefaultValues = {
   password: '',
   confirmPassword: '',
 }
+export const shippingAddressDefaultValues = {
+  fullName: "John Doe",
+  streetAddress: "123 Main st.",
+  city: "anyTown",
+  postalCode: "12345",
+  country: "USA",
+};
