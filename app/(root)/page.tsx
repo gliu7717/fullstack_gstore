d424d8a4373115c18f20ea0cff72b8f7a73e1e17
rayliu7717 <liu.geyang@gmail.com>
2026-02-25 21:29:43 -0500

    add carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
 const delay = (ms: number | undefined) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import { getLatestProducts, getFeaturedProducts } from "@/lib/actions/product.actions";
 
 export const dynamic = "force-dynamic";
 import { Product } from "@/types";
@@ -9,8 +10,14 @@ import { Product } from "@/types";
 const Homepage = async () => {
   //  await delay(2000);
   const latestProducts = await getLatestProducts() as unknown as Product[];
+  const featuredProducts =
+    (await getFeaturedProducts()) as unknown as Product[];
+
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title='Newest Arrivals' />
     </>
   );
