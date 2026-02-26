af7d37cafeb3f7087e30a2e265410f0748b6d35e
rayliu7717 <liu.geyang@gmail.com>
2026-02-25 20:25:59 -0500

    add category drawer

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
 import Menu from "./menu";
+import CategoryDrawer from './category-drawer';
+
 const Header = () => {
   return (
     <header className='w-full border-b'>
       <div className='wrapper flex-between'>
         <div className='flex-start'>
+          <CategoryDrawer />
           <Link href='/' className='flex-start'>
             <Image
               priority={true}
