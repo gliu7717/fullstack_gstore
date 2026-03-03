e3d1fc2e780a20561b9835a5ecd55f733774b15f
rayliu7717 <liu.geyang@gmail.com>
2026-03-02 21:57:12 -0500

    add auth-guard.ts

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -13,13 +13,14 @@ import { BadgeDollarSign, Barcode, CreditCard, Users } from 'lucide-react';
 import { Metadata } from 'next';
 import Link from 'next/link';
 import Charts from './charts';
+import { requireAdmin } from '@/lib/auth-guard';
 
 export const metadata: Metadata = {
     title: 'Admin Dashboard',
 };
 
 const AdminOverviewPage = async () => {
-
+    await requireAdmin();
     const summary = await getOrderSummary();
 
     console.log(summary)
